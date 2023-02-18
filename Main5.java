2144d9913c9abf508ef4402c7306a5da8ad7827f
Cici <48628385+ClosedCaptions@users.noreply.github.com>
2023-02-17 20:50:15 -0500

    Add files via upload

---
+++ Main5.java
@@ -0,0 +1,21 @@
+package thinkingTasks;
+
+public class Main5 {
+
+	public static void main(String[] args) {
+		
+//		for (int a = 1; a <= 6; a++){
+//			for (int b = 1; b <= 6; b++){
+//				for (int c = 1; c <= 6; c++){
+//					for (int d = 1; d <= 6; d++){
+//						for (int e = 1; e <= 6; e++){
+//							for (int f = 1; f <= 6; f++){
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
+	}
+
+}
